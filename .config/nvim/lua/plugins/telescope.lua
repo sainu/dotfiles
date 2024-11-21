da1fa970d3970067794e63d5e2e91c15d9f95164
sainu <katsutoshi.saino@gmail.com>
2024-11-21 14:53:16 +0900

    Change telescope 'layout_strategy' to 'vertical'

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,9 @@ return {
   },
   config = function()
     require('telescope').setup({
+      defaults = {
+        layout_strategy = 'vertical',
+      },
       pickers = {
         find_files = {
           hidden = true,
