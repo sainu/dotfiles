bc737285035be9be3ca6d97416bb9aa445f14a8d
sainu <katsutoshi.saino@gmail.com>
2024-11-21 21:59:10 +0900

    Add keymaps for NeoVim Terminal

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -16,3 +16,7 @@ vim.keymap.set('n', '<leader>s', ':wq<CR>')
 -- Split window
 vim.keymap.set('n', '<C-w>s', ':split<CR><C-w>j', { noremap = true })
 vim.keymap.set('n', '<C-w>v', ':vsplit<CR><C-w>l', { noremap = true })
+
+-- Terminal window
+vim.keymap.set('n', '<leader>t', ':term<CR>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
