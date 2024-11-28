11459a3a12cb805a471d19a01cad2d61242628cf
sainu <katsutoshi.saino@gmail.com>
2024-11-28 15:13:47 +0900

    Hide gitignored files in filter_items

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
-        hide_gitignored = false,
+        hide_gitignored = true,
         never_show = {
           ".DS_Store",
         },
