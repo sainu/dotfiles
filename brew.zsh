15bb4d264e3b94513ec79c63e5ff85b661560e69
sainu <katsutoshi.saino@gmail.com>
2024-12-11 19:18:18 +0900

    Add sd to brew

--- brew.zsh
+++ brew.zsh
@@ -35,6 +35,7 @@ brew upgrade
 brew_install_if_missing bat # cat alternative
 brew_install_if_missing fd # find alternative. Used by telescope.nvim
 brew_install_if_missing eza # ls alternative
+brew_install_if_missing sd # sed alternative
 
 brew_install_if_missing font-hack-nerd-font
 brew_install_if_missing fzf # Fuzzy find history and neovim
